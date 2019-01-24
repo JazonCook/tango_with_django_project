65132f27b25f1f79dd0320233ebba9b56cdfd7ab
Jason Cook <2328261c@student.gla.ac.uk>
2019-01-24 23:00:36 +0000

    Cleanup

--- rango/views.py
+++ rango/views.py
@@ -12,9 +12,8 @@ def index(request):
 	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-	context_dict = {'normalmessage': "Message from views.py!"}
 
-	return render(request, 'rango/about.html', context=context_dict)
+	return render(request, 'rango/about.html')
 	
 def show_category(request, category_name_slug):
 	context_dict = {}
